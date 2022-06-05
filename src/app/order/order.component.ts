48b0dc0916afdcf52a6abbe41a65709b53dfbbd8
uraniumpiggy <nikita.vic.uhin@gmail.com>
2022-06-05 23:11:58 +0300

    order address autocomplete

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export interface AddressTips {
+  type: string,
+  features: {properties: {
+    formatted: string,
+  }}[],
+  query: object,
+}
 
 @Component({
   selector: 'app-order',
@@ -6,10 +15,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
+  address: string = ''
+  addressHints: string[] = []
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  handleAddressChange(value: string) {
+    this.address = value
+    if (!this.address.trim()) {
+      return
+    }
+
+    let params = new HttpParams()
+    params = params.append('text', this.address)
+    params = params.append('lang', 'ru')
+    params = params.append('apiKey', 'c77c8bb0471947ae8fbba8449c614740')
+
+
+    this.http.get<AddressTips>('https://api.geoapify.com/v1/geocode/autocomplete', {
+      params: params
+    })
+      .subscribe(response => {
+        if (response.features.length > 0) {
+          this.addressHints = response.features.map((item) => {
+            return item.properties.formatted
+          })
+        }
+    })
+  }
 }
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"
 import { OrderComponent } from "./order.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { CommonModule } from "@angular/common";
+
 
 @NgModule({
     declarations: [
         OrderComponent
     ],
     imports: [
+        CommonModule,
         FormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        HttpClientModule,
+        MatAutocompleteModule,
     ],
     exports: [
         OrderComponent,
